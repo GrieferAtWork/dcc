a999f626328d6a2f5825805d9be5d5367a1d032f
GrieferAtWork <sagebar@web.de>
2017-06-13 19:25:51 +0200

    Add small wrapper headers to std lib

---
+++ lib/include/sys/fcntl.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2017 Griefer@Work                                            *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement in the product documentation would be  *
+ *    appreciated but is not required.                                        *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ */
+#pragma once
+#pragma GCC system_header
+
+#ifndef __has_include_next
+#define __has_include_next(x) 0
+#endif
+
+#if __has_include_next(<sys/fcntl.h>)
+#include_next <sys/fcntl.h>
+#else
+#include <__stdinc.h>
+__STRICT_ANSI_HEADER
+#include <fcntl.h>
+#endif
